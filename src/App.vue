1256e818f7c8053ea3f5175703535e72237a08d8
otxwg <1987991967@qq.com>
2022-06-13 23:35:46 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<!--
+ * @version: 1.0.0
+ * @Author: otxwg<1987991967@qq.com>
+ * @Date: 2022-06-13 23:30:00
+ * @LastEditTime: 2022-06-13 23:30:00
+-->
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+</style>
